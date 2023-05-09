203a3f654e208a9deb00d160ac71a9033b953169
eddiemuhoro <eddiemuhoro@gmail.com>
2023-05-09 15:00:56 +0300

    added a new file

--- components/home/Recommended.js
+++ components/home/Recommended.js
@@ -1,17 +1,41 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { FlatList, Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
 import React from 'react'
+import { constStyles } from '../../constants'
+import { mentors } from '../../constants'
 
-const Meetings = () => {
+const Recommended = () => {
+
+  const renderItem= ({item})=> (
+    <TouchableOpacity style={constStyles.mentorRectangles}>
+      <View>
+        <View style={[constStyles.profileImage, {marginBottom:22}]}>
+          <Image source={item.pic} />
+        </View>
+        <View>
+          <Text style={constStyles.miniHeader}>{item.name}</Text>
+          <Text style={constStyles.rectangleText}>{item.title}</Text>
+        </View>
+      </View>
+    </TouchableOpacity>
+  )
     
   return (
     <View>
-      <Text>
-        
-      </Text>
+             <Text style={constStyles.miniHeader}>Recommended mentors</Text>
+             <FlatList
+                  data={mentors}
+                  showsHorizontalScrollIndicator={false}
+                  horizontal={true}
+                  renderItem={renderItem}
+                  keyExtractor={item => item.id}
+                  />
+
     </View>
   )
 }
 
-export default Meetings
+export default Recommended
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+
+})
