b2e8c2739636e10222589e08ff5038ee27be717f
eddiemuhoro <eddiemuhoro@gmail.com>
2023-05-10 19:02:27 +0300

    added starr and fav icon

--- components/home/Recommended.js
+++ components/home/Recommended.js
@@ -1,19 +1,25 @@
 import { FlatList, Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
 import React from 'react'
-import { constStyles } from '../../constants'
+import { COLORS, constStyles } from '../../constants'
 import { mentors } from '../../constants'
+import { FontAwesome, MaterialIcons } from '@expo/vector-icons';
 
 const Recommended = () => {
 
   const renderItem= ({item})=> (
     <TouchableOpacity style={constStyles.mentorRectangles}>
       <View>
-        <View style={[constStyles.profileImage, {marginBottom:22}]}>
+        <View style={[constStyles.profileImage, {marginBottom:10}]}>
           <Image source={item.pic} />
         </View>
         <View>
           <Text style={constStyles.miniHeader}>{item.name}</Text>
           <Text style={constStyles.rectangleText}>{item.title}</Text>
+        </View>
+        <MaterialIcons style={{position:'absolute', top:0, right:0}} name="favorite" size={24} color={COLORS.green} />
+        <View style={{flexDirection:'row', alignItems:"center", position:'absolute', bottom:0, right:0}}>
+          <Text style={{marginRight:5}}>4.5</Text>
+          <FontAwesome name="star" size={16} color='#FFC542'/>
         </View>
       </View>
     </TouchableOpacity>
@@ -21,7 +27,6 @@ const Recommended = () => {
     
   return (
     <View>
-             <Text style={constStyles.miniHeader}>Recommended mentors</Text>
              <FlatList
                   data={mentors}
                   showsHorizontalScrollIndicator={false}
